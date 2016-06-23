ec406aff798f6770a276e3e91d9d3775fb236e77
Kate Walters <kawalters01@gmail.com>
2016-06-23 14:38:59 -0500

    two working tests

--- test/test.rb
+++ test/test.rb
@@ -7,4 +7,8 @@ class CurrencyTest < Minitest::Test
   def test_correct_currency
     assert_equal('USD', Currency.new('USD', 100).code)
   end
+
+  def test_correct_amount
+    assert_equal('100', Currency.new('USD', "100").amount)
+  end
 end
