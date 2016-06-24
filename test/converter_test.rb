86aface553d12e7d7bcd6928a21dd0613bcbe988
Kate Walters <kawalters01@gmail.com>
2016-06-24 15:22:18 -0500

    Seven passing Conversion tests.

--- test/converter_test.rb
+++ test/converter_test.rb
@@ -5,7 +5,7 @@ require_relative '../lib/currency_hw.rb'
 class ConverterTest < Minitest::Test
 
 
-  def test_correct_conversion
+  def test_convert_USD_to_JPY_x1
     currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
 
     expect = Currency.new("USD", 1.0)
@@ -15,7 +15,7 @@ class ConverterTest < Minitest::Test
     assert_equal(expect, actual)
   end
 
-  def test_convert_with_different_result
+  def test_convert_USD_to_JPY_x2
     currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
 
     expect = Currency.new("USD", 2.0)
@@ -24,4 +24,55 @@ class ConverterTest < Minitest::Test
 
     assert_equal(expect, actual)
   end
+
+  def test_convert_EUR_to_USD
+    currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
+
+    expect = Currency.new("EUR", 88)
+    starting_currency = Currency.new("USD", 100)
+    actual = currency_converter.convert(starting_currency, :EUR)
+
+    assert_equal(expect, actual)
+  end
+
+  def test_convert_JPY_to_USD
+    currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
+
+    expect = Currency.new("JPY", 106.58)
+    starting_currency = Currency.new("USD", 1.0)
+    actual = currency_converter.convert(starting_currency, :JPY)
+
+    assert_equal(expect, actual)
+  end
+
+  def test_convert_EUR_to_JPY
+    currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
+
+    expect = Currency.new("EUR", 88)
+    starting_currency = Currency.new("JPY", 10658)
+    actual = currency_converter.convert(starting_currency, :EUR)
+
+    assert_equal(expect, actual)
+  end
+
+  def test_convert_USD_to_EUR
+    currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
+
+    expect = Currency.new("USD", 300)
+    starting_currency = Currency.new("EUR", 264)
+    actual = currency_converter.convert(starting_currency, :USD)
+
+    assert_equal(expect, actual)
+  end
+
+  def test_convert_JPY_to_EUR
+    currency_converter = CurrencyConverter.new( { USD: 1.0, EUR: 0.88, JPY: 106.58 } )
+
+    expect = Currency.new("JPY", 106.58)
+    starting_currency = Currency.new("USD", 1.0)
+    actual = currency_converter.convert(starting_currency, :JPY)
+
+    assert_equal(expect, actual)
+  end
+
 end
